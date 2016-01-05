5a4bab0171ae093184deb55a0355c31fa1441775
alanbares <neloice@gmail.com>
2016-01-04 21:10:26 -0800

    Make call to Forismatic api by clicking "Get Quotes" button.

--- static/script.js
+++ static/script.js
@@ -1,9 +1,16 @@
-// Quotes.net Api Call
+/*global $ */
 
-function fetchQuoteData() {
+// Forismatic Api Call
+$("button").click (function() {
     var baseUrl = "http://api.forismatic.com/api/1.0/",
         method = "?method=getQuote",
         lang = "&lang=en",
         format = "&format=json&jsonp=?",
         apiUrl = baseUrl + method + lang + format;
-}
+
+    console.log("url", apiUrl);
+
+    $.getJSON(apiUrl, function (data) {
+       console.log("data", data)
+    });
+});
