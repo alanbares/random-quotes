60c0f70b82ab3cdd7924ae419a0f17106fb291af
alanbares <neloice@gmail.com>
2016-01-05 22:01:31 -0800

    Fix syntax error for format var. Console now returns data properly.

--- static/script.js
+++ static/script.js
@@ -5,7 +5,7 @@ $("button").click (function() {
     var baseUrl = "http://api.forismatic.com/api/1.0/",
         method = "?method=getQuote",
         lang = "&lang=en",
-        format = "&format=json&jsonp=?",
+        format = "&format=jsonp&jsonp=?",
         apiUrl = baseUrl + method + lang + format;
 
     console.log("url", apiUrl);
