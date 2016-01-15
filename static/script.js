89024f18011f5a2af0d68f1bc3a2a3f86abd938d
alanbares <neloice@gmail.com>
2016-01-14 18:49:22 -0800

    Comment out button and handlebars. Display quotes on page without button click.

--- static/script.js
+++ static/script.js
@@ -1,8 +1,26 @@
 /*global $ */
 
 // Forismatic Api Call
-$("button").click (function() {
-    var baseUrl = "http://api.forismatic.com/api/1.0/",
+//$("button").click (function() {
+//    var baseUrl = "http://api.forismatic.com/api/1.0/",
+//        method = "?method=getQuote",
+//        lang = "&lang=en",
+//        format = "&format=jsonp&jsonp=?",
+//        apiUrl = baseUrl + method + lang + format;
+//
+//    console.log("url", apiUrl);
+//
+//    $.getJSON(apiUrl, function (data) {
+//       console.log("data", data)
+//	   $("#randomQuotes").append("<p>" + data.quoteText + "</p>");
+//    });
+//});
+
+$(document).ready(function() {
+	getQuotes();
+
+	function getQuotes() {
+		var baseUrl = "http://api.forismatic.com/api/1.0/",
         method = "?method=getQuote",
         lang = "&lang=en",
         format = "&format=jsonp&jsonp=?",
@@ -12,14 +30,14 @@ $("button").click (function() {
 
     $.getJSON(apiUrl, function (data) {
        console.log("data", data)
-       $("#randomQuotes").append("<p>" + data.quoteText + "</p>");
-    });
+	   $("#randomQuotes").append("<p>" + data.quoteText + "</p>");
+	});
+	}
 });
-
 // Handlebars
-var renderTemplate = function (templateId, targetId, context) {
-    var source = $(templateId).html();
-    var template = Handlebars.compile(source);
-    var html = template(context);
-    $(targetId).append(html);
-}
+//var renderTemplate = function (templateId, targetId, context) {
+//    var source = $(templateId).html();
+//    var template = Handlebars.compile(source);
+//    var html = template(context);
+//    $(targetId).append(html);
+//}
