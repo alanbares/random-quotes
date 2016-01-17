3587a42714faa7e518e2c459fd39e70bafcf375b
alanbares <neloice@gmail.com>
2016-01-16 21:37:48 -0800

    Center text and and non functional tweet button

--- static/script.js
+++ static/script.js
@@ -1,21 +1,5 @@
 /*global $ */
 
-// Forismatic Api Call
-//$("button").click (function() {
-//    var baseUrl = "http://api.forismatic.com/api/1.0/",
-//        method = "?method=getQuote",
-//        lang = "&lang=en",
-//        format = "&format=jsonp&jsonp=?",
-//        apiUrl = baseUrl + method + lang + format;
-//
-//    console.log("url", apiUrl);
-//
-//    $.getJSON(apiUrl, function (data) {
-//       console.log("data", data)
-//	   $("#randomQuotes").append("<p>" + data.quoteText + "</p>");
-//    });
-//});
-
 $(document).ready(function() {
     var author=$("#author"),
         quotes=$("#randomQuotes")
@@ -35,10 +19,10 @@ $(document).ready(function() {
 
         // Displays Unknown if quote has no author
         if (data.quoteAuthor === "") {
-            author.html("- Unknown");
+            author.html("Unknown");
         }
         else {
-            author.html("-" + data.quoteAuthor);
+            author.html(data.quoteAuthor);
         }
 	});
 	}
@@ -47,6 +31,7 @@ $(document).ready(function() {
 		getQuotes();
 	});
 });
+
 // Handlebars
 //var renderTemplate = function (templateId, targetId, context) {
 //    var source = $(templateId).html();
